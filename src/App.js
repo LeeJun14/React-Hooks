7997eba0b5cb62c8bbd55619d16f7a9c74422324
DESKTOP-JPOHH97\AEGISECU <777ljy777@gmail.com>
2023-08-30 09:30:50 +0900

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,40 @@
-import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
 
-function App() {
+
+const useInput = (initialValue, vaildator) => {
+
+  const [value, setValue] = useState(initialValue);
+
+  const onChange = event => {
+
+    const {
+      target: { value }
+    } = event;
+
+    let willUpdate = true;
+    if (typeof vaildator === "function") {
+      willUpdate = vaildator(value);
+    }
+    if (willUpdate) {
+      setValue(value)
+    };
+  };
+  return { value, onChange };
+};
+
+const App = () => {
+
+  const maxLen = value => value.length <= 10;
+
+  const name = useInput("Mr.", maxLen);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Hello</h1>
+      <input placeholder='Name' value={name.value} onChange={name.onChange}></input>
     </div>
   );
-}
+};
 
 export default App;
